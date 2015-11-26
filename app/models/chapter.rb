2fdecff5f25c7c28f9c1c9097a6669ed512f863f
jihan2032 <jihan2032@gmail.com>
2015-11-26 12:08:30 +0200

    add genres in novel forms

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -16,10 +16,13 @@
 
 class Chapter < ActiveRecord::Base
   # Relations
-  has_many   :likes_chapters
+  has_many   :liked_chapters
   has_many   :read_chapters
   belongs_to :novel
   belongs_to :user
+  belongs_to :parent, class_name: 'Chapter'
+  has_many   :children, class_name: 'Chapter', foreign_key: 'parent_id'
+
 
   # Validations
   validates :title, presence: true
