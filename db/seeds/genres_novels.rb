1cc231017f585264b0ecfe34a8c6c70065591361
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-11-26 20:05:08 +0200

    modified genres seed data

--- db/seeds/genres_novels.rb
+++ db/seeds/genres_novels.rb
@@ -1,6 +1,5 @@
 puts 'Seeding Novels with genres...'
-for i in 0..20
-  novel = Novel.all[i]
+Novel.all.each do |novel|
   novel.genres << Genre.first
   novel.genres << Genre.last if Genre.count > 1 
 end
