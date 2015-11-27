174ea6bdfcdecd23bd2d0183a076ea33935e7f5c
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-11-27 15:36:40 +0200

    fixed bootstrap issue

--- app/assets/javascripts/application.coffee
+++ app/assets/javascripts/application.coffee
@@ -1,6 +1,3 @@
-#= require jquery
-#= require jquery_ujs
-#= require bootstrap-sprockets
 #= require vendor
 #= require main
 #= require novels
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151126184826) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
   create_table "chapters", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.integer  "chapter_no", limit: 4,     default: 1
