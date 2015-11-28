bd40ec36aa86c2e379673cc2d53ca165e6d92380
jihan2032 <jihan2032@gmail.com>
2015-11-28 10:32:19 +0200

    my readings & writings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [ :show, :edit, :update ]
+  before_action :set_user, only: [ :show, :edit, :update, :my_readings, :my_writings ]
 
   def show
   end
@@ -15,6 +15,14 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def my_readings
+    @novels = @user.read_novels.page params[:page]
+  end
+
+  def my_writings
+    @novels = @user.novels.page params[:page]
   end
 
 private
