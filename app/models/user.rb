4b41e5756bd347def9520f3e025675f44f1bebff
jihan2032 <jihan2032@gmail.com>
2015-12-16 18:17:50 +0200

    sending last read chapter id

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,10 @@ class User < ActiveRecord::Base
     ReadChapter.where(user_id: id, chapter_id: chapter.id).any?
   end
 
+  def last_read_chapter(novel)
+    (novel.chapters & Chapter.where(id: read_chapters.pluck(:chapter_id))).last
+  end
+
   # def viewed_videos
   #   Video.where(id: VideoTrack.where(id: viewed_video_tracks.pluck(:video_track_id)).pluck(:video_id))
   # end
@@ -81,123 +85,4 @@ class User < ActiveRecord::Base
     ViewedVideoTrack.where(user_id: id, video_track_id: track.id).any?
   end
 
-    def adv
-    array = ["ausland",
-    "art",
-    "elite",
-    "ende",
-    "englisch",
-    "spielen",
-    "und",
-    "guten",
-    "zwei",
-    "zweite",
-    "zehn",
-    "vielen",
-    "sprache",
-    "saft",
-    "richtig",
-    "ohne",
-    "nicht",
-    "negative",
-    "möchte",
-    "meine",
-    "machen",
-    "kommen",
-    "kleine",
-    "jahr",
-    "ich",
-    "hilfe",
-    "hause",
-    "habe",
-    "groß",
-    "gesehen",
-    "frau",
-    "unter",
-    "jetzt",
-    "geben",
-    "sollen",
-    "warum",
-    "was",
-    "wasser",
-    "können",
-    "schon",
-    "bitte",
-    "bin",
-    "über",
-    "nach",
-    "sagen",
-    "sehen",
-    "finden",
-    "freund",
-    "arbeiten",
-    "recht",
-    "ende",
-    "frage",
-    "nehmen",
-    "bleiben",
-    "auto",
-    "unendlich",
-    "fleisch",
-    "milch",
-    "weiß",
-    "zucker",
-    "zutaten",
-    "tag",
-    "hause",
-    "schule",
-    "hochschule",
-    "reis",
-    "haare",
-    "finanzsystem",
-    "ja",
-    "exil",
-    "papier",
-    "positiv",
-    "kühlbox",
-    "luxemburg",
-    "explosiv",]
-    new_array = Array.new
-    # array.each do |word|
-    #   new_word = "<s> "
-    #   for i in 0..word.size
-    #     new_word = new_word + "#{word[i]} "
-    #   end
-    #   new_word = new_word + "</s>"
-    #   new_array << new_word
-    # end
-    for i in 0..125
-      new_sen = "<s> "
-      (0..8).each do
-        new_sen = new_sen + "#{array[rand array.size]} "
-      end
-      new_sen = new_sen + "</s>"
-      new_array << new_sen
-    end
-    new_array
-  end
-
-  def write_sentences
-    File.open("sentences").readlines.each_with_index do |line, index|
-      new_file = File.open("trans#{index}.text", "w")
-      new_file.puts line
-      new_file.close
-    end
-  end
-
-  def write_sentences_2
-    File.open("sentences2").readlines.each_with_index do |line, index|
-      new_file = File.open("trans_2_#{index}.text", "w")
-      new_file.puts line
-      new_file.close
-    end
-  end
-
-  def write_words
-    File.open("new_trans").readlines.each_with_index do |line, index|
-      new_file = File.open("word_trans#{index}.text", "w")
-      new_file.puts line
-      new_file.close
-    end
-  end
 end
